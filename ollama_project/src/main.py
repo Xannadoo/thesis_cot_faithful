c473e093b0c675222aa095fe6ca84158fbf3d541
Sanna <ccor@itu.dk>
2025-04-11 21:29:07 +0200

    restart from last pos

--- ollama_project/src/main.py
+++ ollama_project/src/main.py
@@ -6,10 +6,23 @@ from pprint import pprint
 
 def main():
   paths = [os.path.join(dirpath,f) for (dirpath, dirnames, filenames) in os.walk('/data/bbh') for f in filenames] #docker
-  #paths = [os.path.join(dirpath,f) for (dirpath, dirnames, filenames) in os.walk('../ollama_project/data/bbh') for f in filenames]  #laptop
   print("found:", paths)
   output_dir = '/outputs'
+  
+  chat = LlamaChat()
+
   for pp in range(0,len(paths),2):
+    save_path = f'{output_dir}/{chat.model[-4:]}_{paths[pp][10:-22].replace(":","_")}.json'
+
+    if not os.path.exists(save_path):
+      with open(save_path, 'w') as f:
+        print(f"Created {save_path}")
+        c = 0
+    
+    else:
+      with open(save_path, 'r') as f:
+        prev = [json.loads(line) for line in f.readlines()]
+        c = len(prev)
 
     with open(paths[pp]) as json_data:
       few_shot = json.load(json_data)
@@ -18,8 +31,9 @@ def main():
       val_data = json.load(json_data)
       
     print('Data length: ', len(val_data['data']))
+    print('Starting at: ', c)
 
-    for x in range(len(val_data['data'])):
+    for x in range(c, len(val_data['data'])):
       example = {} #hold the outputs for writing out later
       
       inputs = val_data['data'][x]['parsed_inputs']
@@ -62,12 +76,7 @@ def main():
       #print('*'*30)
       #print(f'\nExpected answer: {correct}: {val_data["data"][x]["targets"]}. Possilbe: {val_data["data"][x]["multiple_choice_targets"]}')
 
-      save_path = f'{output_dir}/{chat.model[-4:]}_{paths[pp][10:-22].replace(":","_")}.json'
-
-      if not os.path.exists(save_path):
-        with open(save_path, 'w') as f:
-          print(f"Created {save_path}")
-
+  
       with open(save_path , 'a+') as f:
         f.write(json.dumps(example) + '\n')
         #print(f"Saved {save_path}")
